34d3a55f6b1628725514cd6c76bb52cf4cedc244
Henrique <ap_henrique@hotmail.com>
2022-01-09 00:05:26 -0300

    Fix HomePage to redirecting ProductListPage

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,5 +1,3 @@
-import 'package:desafio_vesti/features/home/widgets/custom_bottom_navigation_bar.dart';
-import 'package:desafio_vesti/features/product/view/pages/product_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,59 +10,13 @@ class HomePage extends StatefulWidget {
 class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Zummedy"),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.settings),
-          )
-        ],
+    Future.delayed(const Duration(seconds: 1)).then(
+      (value) => Navigator.pushNamed(context, "/product_list_page"),
+    );
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextButton(
-                      onPressed: () {}, child: const Text("Categoria Teste")),
-                  TextButton(
-                      onPressed: () {}, child: const Text("Categoria Teste")),
-                  TextButton(
-                      onPressed: () {}, child: const Text("Categoria Teste")),
-                  TextButton(
-                      onPressed: () {}, child: const Text("Categoria Teste")),
-                ],
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Colors.grey.shade200,
-              child: Wrap(
-                direction: Axis.horizontal,
-                alignment: WrapAlignment.spaceBetween,
-                children: const [
-                  ProductPage(),
-                  ProductPage(),
-                  ProductPage(),
-                  ProductPage(),
-                  ProductPage(),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
 }
