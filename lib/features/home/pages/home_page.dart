d442426911008ab6bfc86997f79f4718aa896afb
Henrique <ap_henrique@hotmail.com>
2022-01-11 16:17:53 -0300

    Refactor to remove items's mock

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -13,22 +13,10 @@ class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     Future.delayed(const Duration(seconds: 1)).then(
-      (value) => Navigator.pushNamed(context, "/product_list_page"),
+      (value) => Navigator.pushReplacementNamed(context, "/product_list_page"),
     );
 
-    final product = Product.fromMap({
-      "id": 4,
-      "title": "Mens Casual Slim Fit",
-      "price": 15.99,
-      "description":
-          "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-      "rating": {"rate": 2.1, "count": 430}
-    });
-
-    return Scaffold(
-      // body: ProductPage(product: product),
+    return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
       ),
