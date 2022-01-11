56b0b80e35e2a7a9d118a3b2e54f33f5a27d4aa7
Henrique <ap_henrique@hotmail.com>
2022-01-11 01:21:57 -0300

    Mock to test

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:desafio_vesti/features/product/domain/entities/product.dart';
+import 'package:desafio_vesti/features/product/view/pages/product_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +15,20 @@ class _MyHomePageState extends State<HomePage> {
     Future.delayed(const Duration(seconds: 1)).then(
       (value) => Navigator.pushNamed(context, "/product_list_page"),
     );
-    return const Scaffold(
+
+    final product = Product.fromMap({
+      "id": 4,
+      "title": "Mens Casual Slim Fit",
+      "price": 15.99,
+      "description":
+          "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+      "category": "men's clothing",
+      "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+      "rating": {"rate": 2.1, "count": 430}
+    });
+
+    return Scaffold(
+      // body: ProductPage(product: product),
       body: Center(
         child: CircularProgressIndicator(),
       ),
