c4013023928561c04b0debdf74918bee5f8c0f83
Henrique <ap_henrique@hotmail.com>
2022-01-11 01:17:32 -0300

    Implement tests

--- test/feature/product/domain/usecases/get_product_by_id_usecase_test.dart
+++ test/feature/product/domain/usecases/get_product_by_id_usecase_test.dart
@@ -1,14 +1,17 @@
-import 'package:desafio_vesti/features/product/data/datasource/dio/dio_datasource.dart';
+import 'package:desafio_vesti/features/product/data/datasource/dio/dio_products_datasource.dart';
+import 'package:desafio_vesti/features/product/data/datasource/dio/dio_service.dart';
 import 'package:desafio_vesti/features/product/data/datasource/get_product_datasource.dart';
 import 'package:desafio_vesti/features/product/data/repositories/product_repository_impl.dart';
 import 'package:desafio_vesti/features/product/domain/entities/product.dart';
 import 'package:desafio_vesti/features/product/domain/repositories/product_repository.dart';
 import 'package:desafio_vesti/features/product/domain/usecases/get_product_by_id_usecase.dart';
+import 'package:desafio_vesti/utils/http_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Shoud return a product by id', () async {
-    final GetProductsDatasource datasource = DioDatasource();
+    final HttpService service = DioService();
+    final GetProductsDatasource datasource = DioProductsDatasource(service);
 
     final ProductRepository repository = ProductRepositoryImpl(datasource);
 
